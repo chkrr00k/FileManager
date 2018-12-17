c5bc03941af9fb3032d5520c9910c1769ab23e38
chkrr00k <23009645+chkrr00k@users.noreply.github.com>
2018-12-17 20:57:51 +0000

    Bugfix

--- index.js
+++ index.js
@@ -57,29 +57,3 @@ app.get('/download/:file(*)', (req, res) => {
 var server = http.createServer(app);
 
 server.listen(8888, "0.0.0.0");
-
-/*
-http.createServer(function (req, res) {
-  if (req.url == '/fileupload') {
-    var form = new formidable.IncomingForm();
-    form.parse(req, function (err, fields, files) {
-      var oldpath = files.file.path;
-      var newpath = files.file.name;
-
-      mv(oldpath, newpath, function (err) {
-        if (err) throw err;
-        res.write('File uploaded and moved!');
-        res.end();
-      });
-
- });
-  } else {
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write('<form action="fileupload" method="post" enctype="multipart/form-data">');
-    res.write('<input type="file" name="filetoupload"><br>');
-    res.write('<input type="submit">');
-    res.write('</form>');
-    return res.end();
-  }
-}).listen(8088); 
-*/
